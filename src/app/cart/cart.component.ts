71dbf02e940af7b2c60e117d816c51e2be805da1
DustinCai <32723321+DustinCai@users.noreply.github.com>
2022-12-28 17:46:07 -0800

    Initial commit

---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CartService } from '../cart.service';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css'],
+})
+export class CartComponent {
+  items = this.cartService.getItems(); // sets the items using the CartService
+
+  constructor(private cartService: CartService) {}
+}
